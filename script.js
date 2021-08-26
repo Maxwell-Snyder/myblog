85af49f2d0cd2edd66372f534a38b2f43e26f321
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-08-26 15:38:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+    function share () {
+        // document.getElementById ("share").style.display = "block";
+        document.getElementById ("share").style.transform = "scale(1.0)";
+        document.getElementById ("f").style.display = "none";
+        document.getElementById ("hi").style.display = "block";
+    }
+
+    function hide () {
+        // document.getElementById ("share").style.display = "none";
+        document.getElementById ("share").style.transform = "scale(0.0)";
+        document.getElementById ("home").style.display = "block";
+        document.getElementById ("f").style.display = "block";
+        document.getElementById ("hi").style.display = "none"; 
+    }
+
+    function hide2 () {
+        document.getElementById ("share").style.display = "none";
+    }
