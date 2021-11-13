8f46aafec47c2bbd37c5653148cc7a6e8db2aa84
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-11-12 19:29:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function share () {
-        // document.getElementById ("share").style.display = "block";
         document.getElementById ("share").style.transform = "scale(1.0)";
         document.getElementById ("f").style.display = "none";
         document.getElementById ("hi").style.display = "block";
     }
 
     function hide () {
-        // document.getElementById ("share").style.display = "none";
         document.getElementById ("share").style.transform = "scale(0.0)";
         document.getElementById ("home").style.display = "block";
         document.getElementById ("f").style.display = "block";
@@ -17,20 +15,20 @@ function share () {
         document.getElementById ("share").style.display = "none";
     }
         
-        function copy() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-            alert ("Link copied!");
-}
 
 function top () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
+}
+
+mybutton = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
