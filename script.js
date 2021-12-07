dc9c5a73967bca6baf069577b3c8a4125108aca8
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-07 16:29:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 function share () {
-        document.getElementById ("share").style.transform = "scale(1.0)";
-        document.getElementById ("f").style.display = "none";
-        document.getElementById ("hi").style.display = "block";
-    }
+    document.getElementById ("share").style.transform = "scale(1.0)";
+    document.getElementById ("f").style.display = "none";
+    document.getElementById ("hi").style.display = "block";
+}
 
-    function hide () {
-        document.getElementById ("share").style.transform = "scale(0.0)";
-        document.getElementById ("f").style.display = "block";
-        document.getElementById ("hi").style.display = "none"; 
-    }
+function hide () {
+    document.getElementById ("share").style.transform = "scale(0.0)";
+    document.getElementById ("f").style.display = "block";
+    document.getElementById ("hi").style.display = "none"; 
+}
 
-    function hide2 () {
-        document.getElementById ("share").style.display = "none";
-    }
+function hide2 () {
+    document.getElementById ("share").style.display = "none";
+}
         
-
 function top () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
 }
+
+$(document).ready(function(){
+    $("#showsusword").click(function(){
+        if($(this).is(":checked")){
+        	document.getElementById ("susword").style.filter "blur(0px)";
+        }else if($(this).is(":not(:checked)")){
+        	document.getElementById ("susword").style.filter "blur(5px)";
+        }
+    });
+});
