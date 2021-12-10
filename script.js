d3d241c4b4eaff2f6a0ba74dcff7f47c835b9549
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-10 11:22:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,3 @@ element3.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
 });
-
-/*const noContext = document.getElementById('content');
-
-noContext.addEventListener('contextmenu', e => {
-  e.preventDefault();
-});
-*/
