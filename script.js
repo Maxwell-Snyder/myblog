3f3e9adafdfd587bf3306067645dea6be48c0ec8
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-18 18:10:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ element3.addEventListener("click", () => {
 });
 
 
-var countDownDate = new Date("Jan 18, 2022 18:35:00").getTime();
+var countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
@@ -28,15 +30,20 @@ var x = setInterval(function() {
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("ellimegasus").innerHTML = minutes + "m " + seconds + "s ";
+  document.getElementById("ellimegasus").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("ellimegasus").style.display = "none":
+    document.getElementById("exploed").style.display = "none":
   }
 }, 1000);
