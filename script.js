0c6734a94a7dc1215ea3ce55fdf723ab33064fd5
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-18 18:14:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("ellimegasus").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("explode").style.display = "none";
-  }
-}, 1000);
-
-
 const element = document.getElementById('f')
 
 element.addEventListener("click", () => {
