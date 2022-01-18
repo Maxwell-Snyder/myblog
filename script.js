653a4104373b6378f0fb63f8897d3649e765a962
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-18 18:04:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ element5.addEventListener("click", () => {
     localStoarge.clear();
     localStorage.removeItem('dark-mode');
 });
+var countDownDate = new Date("Jan 18, 2022 18:35:00").getTime();
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hi").style.display = "none":
+  }
+}, 1000);
