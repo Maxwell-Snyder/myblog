8e5f976c4d31c674f044a8b05ba5fe99d5b01dab
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-18 18:06:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,11 +32,11 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("hi").innerHTML = minutes + "m " + seconds + "s ";
+  document.getElementById("ellimegasus").innerHTML = minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("hi").style.display = "none":
+    document.getElementById("ellimegasus").style.display = "none":
   }
 }, 1000);
