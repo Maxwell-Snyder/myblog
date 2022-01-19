37539d1fb9c9ff1fe0e7f8136d256944eb6cdb3f
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-19 06:51:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ element3.addEventListener("click", () => {
   document.documentElement.scrollTop = 0; 
 });
 
+const element4 = document.getElementById('copy')
+
+element4.addEventListener("click", () => {
+  var copyText = document.getElementById("link");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+});
+
