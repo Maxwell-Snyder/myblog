9ded685850f0f96d12cd9189fa66fae7a40a2a29
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-28 07:22:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.onselectstart = () => {
 };
 document.getElementById ("hillo").value = navigator.userAgent;
 if (document.getElementById("hillo").value == "Mozilla/5.0 (X11; CrOS x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36") {
-    location.reload();
+    document.getElementById ("overlay").style.display = "block":
 } else {
     
 }
