b74f82448fcaaa212047c54a054d410046fc2dd6
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-30 20:37:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ document.onselectstart = () => {
     event.preventDefault();
 };
 document.getElementById ("hillo").value = navigator.userAgent;
-if (document.getElementById("hillo").value == " Mozilla/5.0 (iPhone; CPU iPhone OS 15_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1" && navigator.onLine == true) {
+if (document.getElementById("hillo").value == " Mozilla/5.0 (iPhone; CPU iPhone OS 15_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1" || document.getElementById("hillo").value == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36") {
     document.getElementById ("demo").style.display = "none";
 }
