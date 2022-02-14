b24b533db3b4597d636254555347bd0dd0407bac
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-02-14 12:22:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ element.addEventListener("click", () => {
 });
 const element2 = document.getElementById("hide");
 element2.addEventListener("click", () => {
-    document.getElementById("share").style.transform = "scale(0.0)"), (document.getElementById("f").style.display = "block"), (document.getElementById("hi").style.display = "none");
+    document.getElementById("share").style.transform = "scale(0.0)"
+    document.getElementById("f").style.display = "block";
+    document.getElementById("hi").style.display = "none";
 });
 const element3 = document.getElementById("top");
 element3.addEventListener("click", () => {
-    document.body.scrollTop = 0), (document.documentElement.scrollTop = 0);
+    document.body.scrollTop = 0 
+    document.documentElement.scrollTop = 0;
 });
 const element4 = document.getElementById("copy");
 element4.addEventListener("click", () => {
