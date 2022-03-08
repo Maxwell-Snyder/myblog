3505b533d6bbe03063cbb1de9a0c8390462991da
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-03-08 18:41:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const element = document.getElementById("f");
 element.addEventListener("click", () => {
-    document.getElementById("share").style.marginRight = "0px";
+    document.getElementById("share").style.transform = "scale(1.0)";
 });
 const element2 = document.getElementById("hide");
 element2.addEventListener("click", () => {
-    document.getElementById("share").style.marginRight = "-100%"
+    document.getElementById("share").style.transform = "-scale(0.0)"
     document.getElementById("f").style.display = "block";
     document.getElementById("hi").style.display = "none";
 });
