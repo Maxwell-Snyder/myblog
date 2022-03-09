8d8a32b57b8e1471cf8dbddb48c21ae5bf1e01a7
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-03-09 12:44:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ element4.addEventListener("click", () => {
 });
 const element5 = document.getElementById("submit");
 element5.addEventListener("click", () => {
-    localStorage.setItem ("submitted","true");
+    if (document.getElementById ("reviewtext").value != null) {
+        localStorage.setItem ("submitted","true");
+    }
 });
 document.onselectstart = () => {
     event.preventDefault();
